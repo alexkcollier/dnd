4f5e60616f98b7016ae1b10c0667a74b4be7e034
Alex Collier <alexkcollier@gmail.com>
2017-09-21 08:53:57 -0400

    Working on fixing nav breaks

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,23 +1,23 @@
 import React from "react"
 import Link from "gatsby-link"
 import styles from "./nav.module.css"
-// import { grids } from "purecss"
-var grid = require("purecss/build/grids-responsive.css")
+import { grids } from "purecss"
+// var grid = require("purecss/build/grids-responsive.css")
 
 export default ({ children }) =>
-  <div className= {`pure-u-lg-1-4 pure-u-1 ${styles.nav}`}>
-    <div className={styles.header}>
-      <div>
-        <Link to={`/`}>
+  <div className={`pure-u-1-4 ${styles.nav}`}>
+    <div className={`pure-u-1 ${styles.header}`}>
+      <div className={`pure-u-1`}>
           <h1>
+        <Link to={`/`}>
             {children}
-          </h1>
         </Link>
-        <Link to={`/races/`}>Races</Link>
-        <Link to={`/classes/`}>Classes</Link>
-        <Link to={`/religion/`}>Religion</Link>
-        <Link to={`/time/`}>Time</Link>
-        <Link to={`/additional-rules/`}>Additional Rules</Link>
+          </h1>
+        <Link className={styles.link} to={`/races/`}>Races</Link>
+        <Link className={styles.link} to={`/classes/`}>Classes</Link>
+        <Link className={styles.link} to={`/religion/`}>Religion</Link>
+        <Link className={styles.link} to={`/time/`}>Time</Link>
+        <Link className={styles.link} to={`/additional-rules/`}>Additional Rules</Link>
       </div>
     </div>
   </div>
