6e0ef37d325ae7246126192294223ad20f601328
Alex Collier <alexkcollier@gmail.com>
2017-12-23 21:25:38 -0500

    add theme color

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'D\&D custom rules' }
+      { hid: 'description', name: 'description', content: 'D\&D custom rules' },
+      { name: 'theme-color', content: '#3d4f5d' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
