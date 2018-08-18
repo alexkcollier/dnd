f6c12194dfd824337a55e2cf883a450b114dfcff
Alex Collier <alexkcollier@gmail.com>
2018-08-18 11:13:41 -0400

    updates packages; adds prismic, prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,39 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
   env: {
     browser: true,
     node: true
   },
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
+  extends: [
+    'standard',
+    'plugin:prettier/recommended',
+    'plugin:vue/recommended'
   ],
+  // required to lint *.vue files
+  plugins: ['prettier', 'vue'],
   // add your custom rules here
-  rules: {},
-  globals: {}
+  rules: {
+    semi: [2, 'never'],
+    'no-console': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true
+      }
+    ],
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 3,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ]
+  }
 }
